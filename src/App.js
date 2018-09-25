206f39ac0256d8928514ebf89a4f167b267d2c6c
saileshkasaju <leosailesh@gmail.com>
2018-09-25 10:34:50 +0545

    convert to class and add change handler

--- src/App.js
+++ src/App.js
@@ -6,25 +6,40 @@ const Pet = props => {
   ]);
 };
 
-const App = () => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, 'Adopt me!'),
-    React.createElement(Pet, {
-      name: 'Luna',
-      animal: 'Dog',
-      breed: 'Havanese',
-    }),
-    React.createElement(Pet, {
-      name: 'Pepper',
-      animal: 'bird',
-      breed: 'Cockateil',
-    }),
-    React.createElement(Pet, {
-      name: 'Doink',
-      animal: 'Cat',
-      breed: 'Mixed',
-    }),
-  ]);
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleTitleClick = this.handleTitleClick.bind(this);
+  }
+  handleTitleClick() {
+    alert('You clicked the title');
+  }
+  render() {
+    return React.createElement('div', {}, [
+      React.createElement(
+        'h1',
+        {
+          onClick: this.handleTitleClick,
+        },
+        'Adopt me!',
+      ),
+      React.createElement(Pet, {
+        name: 'Luna',
+        animal: 'Dog',
+        breed: 'Havanese',
+      }),
+      React.createElement(Pet, {
+        name: 'Pepper',
+        animal: 'bird',
+        breed: 'Cockateil',
+      }),
+      React.createElement(Pet, {
+        name: 'Doink',
+        animal: 'Cat',
+        breed: 'Mixed',
+      }),
+    ]);
+  }
+}
 
 ReactDOM.render(React.createElement(App), document.getElementById('root'));
